a4c8a952e64e9e9120a4c68a3536227d320dbc75
mubarak7785 <mubarakjinabade07@gmail.com>
2022-06-23 11:51:30 +0530

    heading added

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
 
   return (
     <div className="App">
+      <h1>Heading</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello Vite + React!</p>
