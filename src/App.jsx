c5351fca711b2e79887b9fa6d3dad349469d8dc7
mubarak7785 <mubarakjinabade07@gmail.com>
2022-06-25 23:15:38 +0530

    Functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,48 @@ function App() {
 
   const [name, setName] = useState("");
 
+  const [wdata,setWdata]=useState({});
+  const [fcast,setFcast]=useState([])
+
   const getcityData = () => {
     fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=5b4a06620b73c08aedbbecb0e9e88c25&units=metric`
     )
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => {if(data.main.temp){setWdata(data.main);forecast()} })
   };
+
+  const forecast=()=>{
+    fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${name}&cnt=7&appid=5b4a06620b73c08aedbbecb0e9e88c25&units=metric`
+    ).then((response)=>response.json())
+    .then((fcast)=>{setFcast(fcast.list);})
+  }
 
   const getname = (e) => {
     setName(e.target.value);
   };
-  console.log(name);
+ 
   return (
     <div className="App">
       <div className="main-div">
         <div className="inp">
         <input type="text" onChange={getname} placeholder=" Enter city name" />
         <button onClick={getcityData}>Search</button>
+        </div>
+        <div className="display">
+       <p>{wdata.temp}</p>
+        </div>
+        <div>
+          {fcast.map((elem)=>{
+              return(
+                <div>
+                  <img src={`http://openweathermap.org/img/wn/${elem.weather[0].icon}@2x.png`} />
+                  <p>Tempmax: {elem.main.temp_max}</p>
+                  <p>Tempmin: {elem.main.temp_min}</p>
+                </div>
+              )
+          })}
         </div>
        
       </div>
