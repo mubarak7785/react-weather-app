9685250e3ebecc5001f54909a901902f245f49d0
mubarak7785 <mubarakjinabade07@gmail.com>
2022-08-17 23:07:33 +0530

    Sunrise & sunset time added

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,39 @@
 import { useState, useEffect } from "react";
 import GetUserLocation from "./components/Location";
 import "./App.css";
+import {
+  ResponsiveContainer,
+  AreaChart,
+  Area,
+  XAxis,
+  Tooltip,
+  // linearGradient,
+  CartesianGrid,
+} from "recharts";
 
 function App() {
   const [data, setData] = useState({});
+  
 
   const [name, setName] = useState("kudachi");
 
   const [wdata, setWdata] = useState({});
   const [fcast, setFcast] = useState([]);
   const [edata, setEdata] = useState({});
+  const [cordData, setCordData] = useState([]);
   const local = GetUserLocation();
 
   useEffect(() => {
-    getcityData();
+    getcityData(); 
   }, []);
+
+  const convertString = (d) => {
+    return new Date(d * 1000).getHours();
+  };
 
   const getcityData = () => {
     fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=5b4a06620b73c08aedbbecb0e9e88c25&units=metric`
+      `https://api.openweathermap.org/data/2.5/weather?q=${name}&appid=5b4a06620b73c08aedbbecb0e9e88c25&units=metric&exclude=hourly,minutely`
     )
       .then((response) => response.json())
       .then((data) => {
@@ -29,6 +44,7 @@ function App() {
         }
       });
     // console.log(wdata.main.temp)
+    console.log(edata)
   };
 
   const forecast = () => {
@@ -44,6 +60,7 @@ function App() {
   const getname = (e) => {
     setName(e.target.value);
   };
+  
 
   return (
     <div className="App">
@@ -63,6 +80,17 @@ function App() {
             <h2>Humidity : {wdata.humidity} %</h2>
             <h2>Pressure : {wdata.pressure} hPa</h2>
           </div>
+          <div className="ph">
+          <h2>Sunrise :  {`${convertString(edata.sys?.sunrise)}:${new Date(
+                    edata.sys?.sunrise
+                  ).getMinutes()} am`}</h2>
+            <h2>Sunset : {`${convertString(edata.sys?.sunset)-12}:${new Date(
+                    edata.sys?.sunset
+                  ).getMinutes()} pm`}</h2>
+          </div>
+          
+          
+          
         </div>
         <div className="forecast-div">
           {fcast.map((el) => {
